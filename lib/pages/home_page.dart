28f58eb65dac93ae1116ae5df54a785f54fdaef8
crpoudyal <crpoudyal@gmail.com>
2022-09-22 14:36:40 +0545

    feat: refreshIndector added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -28,69 +28,83 @@ class _HomePageState extends State<HomePage> {
         ),
         drawer: const DrawerWidget(),
         body: Obx(
-          () => ListView.builder(
-            physics: const BouncingScrollPhysics(
-              parent: AlwaysScrollableScrollPhysics(),
-            ),
-            itemCount: newsController.newsList.length,
-            itemBuilder: (context, item) {
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: GestureDetector(
-                  onTap: () async {
-                    Uri url = Uri.parse(newsController.newsList[item].url);
-
-                    if (await canLaunchUrl(url)) {
-                      await launchUrl(url);
-                    }
+          () => newsController.newsList.isEmpty
+              ? const Center(
+                  child: CircularProgressIndicator(),
+                )
+              : RefreshIndicator(
+                  onRefresh: () async {
+                    print("reloded");
+                    newsController.getNews();
                   },
-                  child: Card(
-                    elevation: 10,
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                  child: ListView.builder(
+                    physics: const BouncingScrollPhysics(
+                      parent: AlwaysScrollableScrollPhysics(),
                     ),
-                    child: Column(
-                      children: [
-                        ListTile(
-                          title: ClipRRect(
-                            borderRadius: BorderRadius.circular(12),
-                            child: Image(
-                              image: NetworkImage(
-                                  newsController.newsList[item].urlToImage),
+                    itemCount: newsController.newsList.length,
+                    itemBuilder: (context, item) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: GestureDetector(
+                          onTap: () async {
+                            Uri url =
+                                Uri.parse(newsController.newsList[item].url);
+
+                            if (await canLaunchUrl(url)) {
+                              await launchUrl(url);
+                            }
+                          },
+                          child: Card(
+                            elevation: 10,
+                            shape: const RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(12)),
+                            ),
+                            child: Column(
+                              children: [
+                                ListTile(
+                                  title: ClipRRect(
+                                    borderRadius: BorderRadius.circular(12),
+                                    child: Image(
+                                      image: NetworkImage(newsController
+                                          .newsList[item].urlToImage),
+                                    ),
+                                  ),
+                                  subtitle: Column(
+                                    children: [
+                                      const SizedBox(
+                                        height: 10,
+                                      ),
+                                      Text(
+                                        newsController.newsList[item].title
+                                            .toString(),
+                                        style: const TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.black),
+                                      ),
+                                      const SizedBox(
+                                        height: 10,
+                                      ),
+                                      Text(
+                                        newsController
+                                            .newsList[item].description
+                                            .toString(),
+                                        style: const TextStyle(
+                                          fontSize: 14,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
-                          subtitle: Column(
-                            children: [
-                              const SizedBox(
-                                height: 10,
-                              ),
-                              Text(
-                                newsController.newsList[item].title.toString(),
-                                style: const TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black),
-                              ),
-                              const SizedBox(
-                                height: 10,
-                              ),
-                              Text(
-                                newsController.newsList[item].description
-                                    .toString(),
-                                style: const TextStyle(
-                                  fontSize: 14,
-                                ),
-                              ),
-                            ],
-                          ),
                         ),
-                      ],
-                    ),
+                      );
+                    },
                   ),
                 ),
-              );
-            },
-          ),
         ));
   }
 }
