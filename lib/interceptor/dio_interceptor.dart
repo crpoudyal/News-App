40bca6b18e304b925796ea1bd17166e38d2aa07b
crpoudyal <crpoudyal@gmail.com>
2022-10-13 11:16:47 +0545

    feat: printing headers

---
+++ lib/interceptor/dio_interceptor.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+
+import 'package:dio/dio.dart';
+
+class DioInterceptor extends Interceptor {
+  @override
+  FutureOr<dynamic> onRequest(
+      RequestOptions options, RequestInterceptorHandler handler) async {
+    handler.next(options);
+    print('Header Options : ${options.method}');
+    print(
+        "--> ${options.method != null ? options.method.toUpperCase() : 'METHOD'} ${"${options.baseUrl ?? ""}${options.path ?? ""}"}");
+    print("Headers:");
+    options.headers.forEach((k, v) => print('$k: $v'));
+
+    print("queryParameters:");
+    options.queryParameters.forEach((k, v) => print('$k: $v'));
+    if (options.data != null) {
+      print("Body: ${options.data}");
+    }
+    return options;
+  }
+
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
+    print(
+        "<-- ${response.statusCode} ${(response.requestOptions != null ? (response.requestOptions.baseUrl + response.requestOptions.path) : 'URL')}");
+    print("Headers:");
+    response.headers?.forEach((k, v) => print('$k: $v'));
+    print("Response: ${response.data}");
+  }
+
+  @override
+  void onError(DioError err, ErrorInterceptorHandler handler) {
+    print("${err.response != null ? err.response?.data : 'Unknown Error'}");
+  }
+}
