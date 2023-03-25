e5eae99b8b46dffa7121ae545573d4839801aa42
crpoudyal <crpoudyal@gmail.com>
2023-03-25 21:37:24 +0545

    localization using getx

---
+++ lib/res/getx_localization/languages.dart
@@ -0,0 +1,8 @@
+import 'package:get/get_navigation/src/root/internacionalization.dart';
+
+class Languages extends Translations {
+  @override
+  Map<String, Map<String, String>> get keys => {
+        'np_NEP': {'Hello': 'नमस्कार', 'Google': 'गूगल'}
+      };
+}
