1c0f5197286178249fd079dee67e441075d26fc0
crpoudyal <crpoudyal@gmail.com>
2023-04-01 15:01:04 +0545

    app urls

---
+++ lib/res/app_url.dart
@@ -0,0 +1,7 @@
+class AppUrl {
+  static var baseUrl = 'https://newsapi.org/';
+  static var tech =
+      '${baseUrl}v2/top-headlines?sources=techcrunch&apiKey=f6330cdff6244a53b8b7e0082020c8e2';
+  static var business =
+      '${baseUrl}v2/top-headlines?country=us&category=business&apiKey=f6330cdff6244a53b8b7e0082020c8e2';
+}
